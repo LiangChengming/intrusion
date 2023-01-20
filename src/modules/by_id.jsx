bf043117d2656c0954942e92ba620b3b90653bbc
loadi <loadi@x.com>
2023-01-20 11:53:21 +0800

    reaname

--- src/modules/by_id.jsx
+++ src/modules/by_id.jsx
@@ -6,7 +6,7 @@ import geo from "../assets/geo.svg";
 import badguy from "../assets/badguy.svg";
 
 import "./css/by_id.css";
-import { SimpleChart } from "./barChart";
+import { SimpleChart } from "./simpleChart";
 
 export const ChartById = (props) => {
   return (
@@ -17,7 +17,7 @@ export const ChartById = (props) => {
           <div className="id-data-headline"> 地域分布 </div>
         </div>
         <GeoChart className="geo-chart" />
-        <SimpleChart type="bar" className="bar-chart" />
+        <SimpleChart type="bar" className="simple-chart" />
       </div>
 
       <div className="id-inner-section">
@@ -25,7 +25,7 @@ export const ChartById = (props) => {
           <img src={geo} className="id-data-head-icon"></img>
           <div className="id-data-headline"> 人群属性 </div>
         </div>
-        <SimpleChart type="line" className="line-chart" />
+        <SimpleChart type="line" className="simple-chart" />
       </div>
 
       <div className="id-inner-section">
@@ -33,7 +33,7 @@ export const ChartById = (props) => {
           <img src={geo} className="id-data-head-icon"></img>
           <div className="id-data-headline"> 人群属性 </div>
         </div>
-        <SimpleChart type="line" className="line-chart" />
+        <SimpleChart type="line" className="simple-chart" />
       </div>
 
       <div className="id-inner-section">
@@ -41,7 +41,7 @@ export const ChartById = (props) => {
           <img src={badguy} className="id-data-head-icon"></img>
           <div className="id-data-headline"> 危险ID </div>
         </div>
-        <SimpleChart type="line" className="line-chart" />
+        <SimpleChart type="line" className="simple-chart" />
       </div>
     </div>
   );
