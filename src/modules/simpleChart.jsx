039827ac22081836e481dc4772d39896d8913648
loadi <loadi@x.com>
2023-01-20 21:56:10 +0800

    show real value

--- src/modules/simpleChart.jsx
+++ src/modules/simpleChart.jsx
@@ -2,20 +2,38 @@ import React from "react";
 import ReactEcharts from "echarts-for-react";
 
 export const SimpleChart = (props) => {
+  // data of x
   const categories = {
     type: "category",
     data: ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"],
   };
+
   const valueType = {
     type: "value",
   };
 
+  // data of y
   const seriesData = [400, 300, 350, 200, 280];
 
   const options = {
     grid: { top: 20, right: 40, bottom: 20, left: 40, containLabel: true },
     xAxis: props.type != "hbar" ? categories : valueType,
-    yAxis: props.type == "hbar" ? categories : valueType,
+    yAxis:
+      props.type == "hbar"
+        ? [
+            categories,
+            {
+              type: "category",
+              data: seriesData,
+              axisLine: {
+                show: false,
+              },
+              axisTick: {
+                show: false,
+              },
+            },
+          ]
+        : valueType,
     series: [
       {
         data: seriesData,
