81aa540ea1ef289845831dab91e6b4ad55ba076d
loadi <loadi@x.com>
2023-01-30 00:42:52 +0800

    add replay btn

--- src/modules/sub_ai_replay.jsx
+++ src/modules/sub_ai_replay.jsx
@@ -11,7 +11,7 @@ export const SubAiReplayPage = (props) => {
       avatar,
       nickname: "小肚子二三事",
       value:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ut accusantium amet distinctio?",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ut accusaum amet ?",
       level: 1,
       sign: "23-1-6 08:36 来自上海",
       subreplays: [
@@ -53,6 +53,12 @@ export const SubAiReplayPage = (props) => {
     },
   ];
 
+  const replayBtn = (
+    <div className="w-[80px] h-[30px] text-sm bg-earth-green flex justify-center items-center rounded-md">
+      Replay
+    </div>
+  );
+
   var comments = [];
   for (var j = 0; j < 3; j++) {
     var subcomments = [];
@@ -63,34 +69,40 @@ export const SubAiReplayPage = (props) => {
       for (var k = 0; k < fake.subreplays.length; k++) {
         const subfake = fake.subreplays[k];
         level2comments.push(
-          <div className="mt-3">
-            <div className="flex justify-start items-center text-xs">
-              <label className=" text-neon-red">{subfake.nickname}:</label>
-              <label>{subfake.value}</label>
+          <div className="mt-2 flex justify-between items-start w-[650px] hover:bg-earth-green/10">
+            <div className="">
+              <div className="flex justify-start items-center text-xs">
+                <label className=" text-neon-red">{subfake.nickname}:</label>
+                <label>{subfake.value}</label>
+              </div>
+              <div className="text-xs text-[#aaa] mt-1">{subfake.sign}</div>
             </div>
-            <div className="text-xs text-[#aaa]">{subfake.sign}</div>
+            {replayBtn}
           </div>
         );
       }
 
       subcomments.push(
-        <div className="flex flex-col justify-start items-start">
-          <div className="flex justify-start items-center m-3">
-            <img
-              src={fake.avatar}
-              className=" rounded-full w-[35px] h-[35px]"
-            />
-            <div className="flex justify-start items-start flex-col pl-3">
-              <div className="flex justify-start items-center text-xs">
-                <label className=" text-neon-red">{fake.nickname}:</label>
-                <label>{fake.value}</label>
+        <div className=" relative">
+          <div className="flex flex-col justify-start items-start max-w-[700px]">
+            <div className="flex justify-start items-center mt-2">
+              <img
+                src={fake.avatar}
+                className=" rounded-full w-[35px] h-[35px]"
+              />
+              <div className="flex justify-start items-start flex-col pl-3">
+                <div className="flex justify-start items-center text-xs">
+                  <label className=" text-neon-red">{fake.nickname}:</label>
+                  <label>{fake.value}</label>
+                </div>
+                <div className="text-xs text-[#aaa] mt-1">{fake.sign}</div>
               </div>
-              <div className="text-xs text-[#aaa]">{fake.sign}</div>
+            </div>
+            <div className="flex justify-start items-start flex-col pl-3 border-l-2 ml-16">
+              {level2comments}
             </div>
           </div>
-          <div className="flex justify-start items-start flex-col pl-3 border-l-2 ml-16">
-            {level2comments}
-          </div>
+          <div className=" absolute top-2 right-4">{replayBtn}</div>
         </div>
       );
     }
