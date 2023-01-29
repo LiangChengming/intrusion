5feedc4aab5b756a4ad34d76eafb5ba94a5afdf4
loadi <loadi@x.com>
2023-01-29 23:18:01 +0800

    add bottom icons

--- src/modules/ai_replay.jsx
+++ src/modules/ai_replay.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import avatar from "../assets/avatar.png";
 import { Tabular } from "./table";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import IosShareOutlinedIcon from "@mui/icons-material/IosShareOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 
 export const AiReplayPage = (props) => {
   const fake = {
@@ -17,7 +20,7 @@ export const AiReplayPage = (props) => {
   var poster = [];
   for (var i = 0; i < 10; i++) {
     poster.push(
-      <div className="h-[220px] min-h-[220px] bg-white p-2 m-2 box-border hover:bg-pastel-green">
+      <div className="h-[220px] min-h-[220px] min-w-[500px] bg-white p-2 m-2 box-border hover:bg-pastel-green">
         <div className=" w-full flex justify-between items-center">
           <div className="flex justify-start items-center">
             <img
@@ -36,14 +39,36 @@ export const AiReplayPage = (props) => {
           </div>
         </div>
 
-        <div className=" pl-16 text-shade-black/80">{fake.lorem}</div>
+        <div className=" pl-16 text-shade-black/80 h-[130px] ">
+          {fake.lorem}
+        </div>
+        <div className="w-full h-[20px] flex justify-between items-center px-20">
+          <div>
+            <IosShareOutlinedIcon
+              sx={{ color: "#ccc", width: "15px", height: "15px" }}
+            />
+            <label className="text-[#CCC] text-xs ml-2 ">{fake.share}</label>
+          </div>
+          <div>
+            <ChatBubbleOutlineOutlinedIcon
+              sx={{ color: "#ccc", width: "15px", height: "15px" }}
+            />
+            <label className="text-[#CCC] text-xs ml-2 ">{fake.comment}</label>
+          </div>
+          <div>
+            <ThumbUpOutlinedIcon
+              sx={{ color: "#ccc", width: "15px", height: "15px" }}
+            />
+            <label className="text-[#CCC] text-xs ml-2 ">{fake.like}</label>
+          </div>
+        </div>
       </div>
     );
   }
 
   return (
     <div className="w-full h-full flex justify-start  items-center">
-      <div className=" w-[800px] h-full bg-shade-gray overflow-auto">
+      <div className=" w-[800px] min-w-[500px] h-full bg-shade-gray overflow-auto">
         {poster}
       </div>
       <div className=" w-full h-full relative ">
